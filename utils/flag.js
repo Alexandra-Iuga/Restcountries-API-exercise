2e153d1ce30cf22e4a58212f320459b8ae48baf3
Alexandra-Iuga <100163768+Alexandra-Iuga@users.noreply.github.com>
2022-08-09 14:17:15 +0300

    updated

--- utils/flag.js
+++ utils/flag.js
@@ -1,5 +1,4 @@
 function getFlagIcon(iconCode) {
   let lowercaseIconCode = iconCode.toLowerCase();
-  // console.log(lowercaseIconCode);
   return `https://flagcdn.com/w320/${lowercaseIconCode}.png`;
 }
